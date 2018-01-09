a1ff57bb7416fa6cb1e62977a7858edcf5247f21
魏小雨 <6682220aabc@163.com>
2018-01-09 15:23:35 +0800

    config webpack alias

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,14 @@ const rewireLess = require('react-app-rewire-less-modules');
 const AutoDllPlugin = require('autodll-webpack-plugin');
 
 module.exports = function override(config, env) {
+  config.resolve = {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      'components': path.resolve(__dirname, 'src/components'),
+      'assets': path.resolve(__dirname, 'src/assets')
+    }
+  };
+  
   if (env === "development") {
     config = injectBabelPlugin(["dva-hmr"], config);
   }
--- src/routes/Home/components/index.js
+++ src/routes/Home/components/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
+import logo from 'assets/images/logo.png';
 
 @connect()
 export default class Home extends Component {
 
   render() {
-    return <div>Hello World!</div>
+    return <div><img width="50" src={logo} /> Hello World! </div>
   }
 }
