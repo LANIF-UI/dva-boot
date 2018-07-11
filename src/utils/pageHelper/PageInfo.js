940061575fea2d8594ee33c5fbdd77a6969b532a
weiq <6682220aabc@163.com>
2018-07-11 10:59:44 +0800

    update pageinfo startpage

--- src/utils/pageHelper/PageInfo.js
+++ src/utils/pageHelper/PageInfo.js
@@ -43,6 +43,12 @@ export default class PageInfo {
   startPage(pageNum = 1, pageSize = 10) {
     this.pageNum = pageNum;
     this.pageSize = pageSize;
+    this.size = 0;
+    this.total = 0;
+    this.totalPages = 0;
+    this.list = [];
+    this.filters = {};
+    this.sorts = {};
     return this;
   }
 
@@ -117,6 +123,7 @@ export default class PageInfo {
     return this;
   }
 
+  // deprecate
   send(url, options) {
     const self = this;
     const { pageNum, pageSize, filters, sorts } = this;
